2cacafe485362603c4ed1cbfe6bc52c60cb57fc6
Paweł Wojtuń <panpawelw@pm.me>
2020-04-20 22:54:55 +0200

    Add ExerciseTest

---
+++ test/ExerciseTest.java
@@ -0,0 +1,25 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import pl.pjm77.model.Exercise;
+
+public class ExerciseTest {
+
+    public static final int EXPECTED_ID = 0;
+    public static final String EXPECTED_TITLE = "Whatever title";
+    public static final String EXPECTED_DESCRIPTION = "Whatever description";
+
+    private Exercise exercise;
+
+    @Before
+    public void setup() throws Exception {
+        exercise = new Exercise("Whatever title", "Whatever description");
+    }
+
+    @Test
+    public void testExercise() throws Exception {
+        Assert.assertEquals(EXPECTED_ID, exercise.getId());
+        Assert.assertEquals(EXPECTED_TITLE, exercise.getTitle());
+        Assert.assertEquals(EXPECTED_DESCRIPTION, exercise.getDescription());
+    }
+}
