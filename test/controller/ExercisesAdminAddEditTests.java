fa014814eae91ca5b41a3a33b72fcd84151b4fb9
Paweł Wojtuń <panpawelw@pm.me>
2020-12-17 00:46:50 +0100

    Finish ExercisesAdminAddEdit doGet tests

--- test/controller/ExercisesAdminAddEditTests.java
+++ test/controller/ExercisesAdminAddEditTests.java
@@ -1,13 +1,15 @@
 package controller;
 
+import com.panpawelw.DAO.ExerciseDAO;
 import com.panpawelw.controller.ExercisesAdminAddEdit;
-import mockDAOs.MockExerciseDAO;
+import com.panpawelw.model.Exercise;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.mock.web.MockServletConfig;
 
+import static org.easymock.EasyMock.*;
 import static org.junit.Assert.assertEquals;
 
 public class ExercisesAdminAddEditTests {
@@ -15,25 +17,73 @@ public class ExercisesAdminAddEditTests {
     private MockHttpServletRequest request;
     private MockHttpServletResponse response;
     private ExercisesAdminAddEdit exercisesAdminAddEdit;
+    private final ExerciseDAO mockExerciseDAO = mock(ExerciseDAO.class);
 
     @Before
     public void setup() throws Exception {
         request = new MockHttpServletRequest();
         response = new MockHttpServletResponse();
         exercisesAdminAddEdit = new ExercisesAdminAddEdit();
-        exercisesAdminAddEdit.setExerciseDAO(new MockExerciseDAO());
+        exercisesAdminAddEdit.setExerciseDAO(mockExerciseDAO);
         exercisesAdminAddEdit.init(new MockServletConfig());
     }
 
     @Test
     public void exercisesAdminAddEditGetForwardTest() throws Exception {
         exercisesAdminAddEdit.doGet(request, response);
-        assertEquals("/jsp/exercisesadminaddeditview.jsp", response.getForwardedUrl());
+        assertEquals("/exercisesadminpanel", response.getForwardedUrl());
     }
 
     @Test
     public void exercisesAdminAddEditPostForwardTest() throws Exception {
         exercisesAdminAddEdit.doPost(request, response);
         assertEquals("/exercisesadminpanel", response.getForwardedUrl());
+    }
+
+    @Test
+    public void exercisesAdminAddEditGetIncorrectIntegerParameterTest() throws Exception {
+        request.setParameter("id", "16456");
+        expect(mockExerciseDAO.loadExerciseById(16456)).andReturn(null);
+        incorrectParameterTest();
+    }
+
+    @Test
+    public void exercisesAdminAddEditGetIncorrectIntegerSubZeroParameterTest() throws Exception {
+        request.setParameter("id", "-10");
+        incorrectParameterTest();
+    }
+
+    @Test
+    public void exercisesAdminAddEditGetCharacterParameterTest() throws Exception {
+        request.setParameter("id", "x");
+        incorrectParameterTest();
+    }
+
+    @Test
+    public void exercisesAdminAddEditGetNullParameterTest() throws Exception {
+        request.setParameter("id", "null");
+        incorrectParameterTest();
+    }
+
+    @Test
+    public void exercisesAdminAddEditGetCorrectParameterTest() throws Exception {
+        request.setParameter("id", "5");
+        Exercise expectedExercise = new Exercise(5, "Test title",
+                "Test description");
+        expect(mockExerciseDAO.loadExerciseById(5)).andReturn(expectedExercise);
+        replay(mockExerciseDAO);
+        exercisesAdminAddEdit.doGet(request,response);
+        Exercise returnedExercise = (Exercise) request.getAttribute("exercise");
+        assertEquals(expectedExercise, returnedExercise);
+        assertEquals("/jsp/exercisesadminaddeditview.jsp", response.getForwardedUrl());
+        verify(mockExerciseDAO);
+    }
+
+    private void incorrectParameterTest() throws Exception {
+        replay(mockExerciseDAO);
+        exercisesAdminAddEdit.doGet(request,response);
+        assertEquals("No such exercise exists!", request.getAttribute("errormessage"));
+        assertEquals("/exercisesadminpanel", response.getForwardedUrl());
+        verify(mockExerciseDAO);
     }
 }
