b69f8ab61f7e6b5b6e05c4a8c60e59f023953afd
Paweł Wojtuń <panpawelw@pm.me>
2020-12-31 00:01:01 +0100

    Refactor UserGroupsTests, finish UserGroupsTests

--- test/controller/UserGroupsTests.java
+++ test/controller/UserGroupsTests.java
@@ -1,33 +1,46 @@
 package controller;
 
+import com.panpawelw.DAO.UserGroupDAO;
 import com.panpawelw.controller.UserGroups;
-import mockDAOs.MockUserGroupDAO;
+import com.panpawelw.model.UserGroup;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.mock.web.MockServletConfig;
 
+import java.util.List;
+
+import static misc.TestUtils.createMultipleUserGroups;
+import static org.easymock.EasyMock.*;
 import static org.junit.Assert.assertEquals;
 
 public class UserGroupsTests {
 
-    private MockHttpServletRequest request;
-    private MockHttpServletResponse response;
-    private UserGroups userGroups;
+    private final MockHttpServletRequest request = new MockHttpServletRequest();
+    private final MockHttpServletResponse response  = new MockHttpServletResponse();
+    private final UserGroups userGroups = new UserGroups();
+    private final UserGroupDAO mockUserGroupDAO = mock(UserGroupDAO.class);
 
     @Before
     public void setup() throws Exception {
-        request = new MockHttpServletRequest();
-        response = new MockHttpServletResponse();
-        userGroups = new UserGroups();
-        userGroups.setUserGroupDAO(new MockUserGroupDAO());
+        userGroups.setUserGroupDAO(mockUserGroupDAO);
         userGroups.init(new MockServletConfig());
     }
 
     @Test
-    public void UserGroupsForwardTest() throws Exception {
+    public void userGroupsForwardTest() throws Exception {
         userGroups.doGet(request, response);
         assertEquals("/jsp/usergroupsview.jsp", response.getForwardedUrl());
+    }
+
+    @Test
+    public void userGroupsListsMatch() throws Exception {
+        List<UserGroup> expectedList = createMultipleUserGroups(7);
+        expect(mockUserGroupDAO.loadAllUserGroups()).andReturn(expectedList);
+        replay(mockUserGroupDAO);
+        userGroups.doGet(request, response);
+        assertEquals(expectedList, request.getAttribute("usergroups"));
+        verify(mockUserGroupDAO);
     }
 }
