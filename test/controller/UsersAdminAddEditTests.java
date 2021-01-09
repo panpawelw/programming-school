5dc3a44a6ee7fe11ce08a769cf345ac5abb40adb
Paweł Wojtuń <panpawelw@pm.me>
2021-01-10 00:43:56 +0100

    Finish UserAdminAddEdit tests

--- test/controller/UsersAdminAddEditTests.java
+++ test/controller/UsersAdminAddEditTests.java
@@ -32,53 +32,190 @@ public class UsersAdminAddEditTests {
     }
 
     @Test
+    public void usersAdminAddEditGetNotExistingUserIdTest() throws Exception {
+        request.setParameter("id", "16456");
+        expect(mockUserDAO.loadUserById(16456)).andReturn(null);
+        testGetMethod("/usersadminpanel", "No such user exists!");
+    }
+
+    @Test
+    public void usersAdminAddEditGetNewUserTest() throws Exception {
+        request.setParameter("id", "0");
+        testGetMethod("/jsp/usersadminaddeditview.jsp", null,
+                new User(0, null, null, null, 0));
+    }
+
+    @Test
+    public void usersAdminAddEditGetExistingUserTest() throws Exception {
+        request.setParameter("id", "5");
+        User expectedUser = new User(5, "Test name", "Test email",
+                "Test password", 5);
+        expect(mockUserDAO.loadUserById(5)).andReturn(expectedUser);
+        testGetMethod("/jsp/usersadminaddeditview.jsp",
+                null, expectedUser);
+    }
+
+    @Test
     public void usersAdminAddEditPostForwardTest() throws Exception {
         usersAdminAddEdit.doPost(request, response);
         assertEquals("/usersadminpanel", response.getForwardedUrl());
     }
 
     @Test
-    public void usersAdminAddEditGetIncorrectIntegerParameterTest() throws Exception {
-        request.setParameter("id", "16456");
-        expect(mockUserDAO.loadUserById(16456)).andReturn(null);
-        incorrectParameterTest();
+    public void usersAdminAddEditPostIncorrectIdParameterTest() throws Exception {
+        request.setParameter("id", "xxx");
+        request.setParameter("name", "Test name");
+        request.setParameter("email", "Test email");
+        request.setParameter("password", "Test password");
+        request.setParameter("group_id", "1");
+        testPostMethod("Incorrect parameters!");
     }
 
     @Test
-    public void usersAdminAddEditGetIncorrectIntegerSubZeroParameterTest() throws Exception {
-        request.setParameter("id", "-10");
-        incorrectParameterTest();
+    public void usersAdminAddEditPostNullIdParameterTest() throws Exception {
+        request.setParameter("name", "Test name");
+        request.setParameter("email", "Test email");
+        request.setParameter("password", "Test password");
+        request.setParameter("group_id", "1");
+        testPostMethod("Incorrect parameters!");
     }
 
     @Test
-    public void usersAdminAddEditGetCharacterParameterTest() throws Exception {
-        request.setParameter("id", "x");
-        incorrectParameterTest();
+    public void usersAdminAddEditPostEmptyNameParameterTest() throws Exception {
+        request.setParameter("id", "1");
+        request.setParameter("name", "");
+        request.setParameter("email", "Test email");
+        request.setParameter("password", "Test password");
+        request.setParameter("group_id", "1");
+        testPostMethod("Incorrect parameters!");
     }
 
     @Test
-    public void usersAdminAddEditGetNullParameterTest() throws Exception {
-        request.setParameter("id", "null");
-        incorrectParameterTest();
+    public void usersAdminAddEditPostNullNameParameterTest() throws Exception {
+        request.setParameter("id", "1");
+        request.setParameter("email", "Test email");
+        request.setParameter("password", "Test password");
+        request.setParameter("group_id", "1");
+        testPostMethod("Incorrect parameters!");
     }
 
     @Test
-    public void usersAdminAddEditGetCorrectParameterTest() throws Exception {
-        request.setParameter("id", "5");
-        User expectedUser = new User(5, "Test name", "Test email",
-                "Test password", 1);
-        expect(mockUserDAO.loadUserById(5)).andReturn(expectedUser);
+    public void usersAdminAddEditPostEmptyEmailParameterTest() throws Exception {
+        request.setParameter("id", "1");
+        request.setParameter("name", "Test name");
+        request.setParameter("email", "");
+        request.setParameter("password", "Test password");
+        request.setParameter("group_id", "1");
+        testPostMethod("Incorrect parameters!");
+    }
+
+    @Test
+    public void usersAdminAddEditPostNullEmailParameterTest() throws Exception {
+        request.setParameter("id", "1");
+        request.setParameter("name", "Test name");
+        request.setParameter("password", "Test password");
+        request.setParameter("group_id", "1");
+        testPostMethod("Incorrect parameters!");
+    }
+
+    @Test
+    public void usersAdminAddEditPostEmptyPasswordParameterTest() throws Exception {
+        request.setParameter("id", "1");
+        request.setParameter("name", "Test name");
+        request.setParameter("email", "Test email");
+        request.setParameter("password", "");
+        request.setParameter("group_id", "1");
+        testPostMethod("Incorrect parameters!");
+    }
+
+    @Test
+    public void usersAdminAddEditPostNullPasswordParameterTest() throws Exception {
+        request.setParameter("id", "1");
+        request.setParameter("name", "Test name");
+        request.setParameter("email", "Test email");
+        request.setParameter("group_id", "1");
+        testPostMethod("Incorrect parameters!");
+    }
+
+    @Test
+    public void usersAdminAddEditPostIncorrectGroupIdParameterTest() throws Exception {
+        request.setParameter("id", "1");
+        request.setParameter("name", "Test name");
+        request.setParameter("email", "Test email");
+        request.setParameter("password", "Test password");
+        request.setParameter("group_id", "abc");
+        testPostMethod("Incorrect parameters!");
+    }
+
+    @Test
+    public void usersAdminAddEditPostNullGroupIdParameterTest() throws Exception {
+        request.setParameter("id", "1");
+        request.setParameter("name", "Test name");
+        request.setParameter("email", "Test email");
+        request.setParameter("password", "Test password");
+        testPostMethod("Incorrect parameters!");
+    }
+
+    @Test
+    public void usersAdminAddEditPostNewUserTest() throws Exception {
+        request.setParameter("id", "0");
+        request.setParameter("name", "Test name");
+        request.setParameter("email", "Test email");
+        request.setParameter("password", "Test password");
+        request.setParameter("group_id", "5");
+        expect(mockUserDAO.saveUserToDB(new User(0, "Test name", "Test email",
+                anyString(), 5))).andReturn(1);
+        testPostMethod(null);
+    }
+
+    @Test
+    public void usersAdminAddEditPostExistingUserTest() throws Exception {
+        request.setParameter("id", "1");
+        request.setParameter("name", "Test name");
+        request.setParameter("email", "Test email");
+        request.setParameter("password", "Test password");
+        request.setParameter("group_id", "5");
+        expect(mockUserDAO.loadUserById(1)).andReturn(new User(1, "Test name",
+                "Test email", "Test password", 1));
+        expect(mockUserDAO.saveUserToDB(new User(1, "Test name", "Test email",
+                anyString(), 1))).andReturn(1);
+        testPostMethod(null);
+    }
+
+    @Test
+    public void usersAdminAddEditPostDatabaseErrorTest() throws Exception {
+        request.setParameter("id", "1");
+        request.setParameter("name", "Test name");
+        request.setParameter("email", "Test email");
+        request.setParameter("password", "Test password");
+        request.setParameter("group_id", "5");
+        expect(mockUserDAO.loadUserById(1)).andReturn(new User(1, "Test name",
+                "Test email", "Test password", 1));
+        expect(mockUserDAO.saveUserToDB(new User(1, "Test name", "Test email",
+                anyString(), 1))).andReturn(0);
+        testPostMethod("Database error!");
+    }
+
+    private void testGetMethod(String expectedUrl, String expectedErrorMessage,
+                               User... expectedUser) throws Exception {
         replay(mockUserDAO);
         usersAdminAddEdit.doGet(request,response);
-        assertEquals(expectedUser, request.getAttribute("user"));
-        assertEquals("/jsp/usersadminaddeditview.jsp", response.getForwardedUrl());
+        assertEquals(expectedUrl, response.getForwardedUrl());
+        if (expectedErrorMessage != null) {
+            assertEquals(expectedErrorMessage, request.getAttribute("errormessage"));
+        }
+        if (expectedUser.length > 0) {
+            assertEquals(expectedUser[0], request.getAttribute("user"));
+        }
         verify(mockUserDAO);
     }
 
-    private void incorrectParameterTest() throws Exception {
+    private void testPostMethod(String errorMessage) throws Exception {
         replay(mockUserDAO);
-        usersAdminAddEdit.doGet(request,response);
-        assertEquals("No such user exists!", request.getAttribute("errormessage"));
+        usersAdminAddEdit.doPost(request, response);
+        if (errorMessage != null) {
+            assertEquals(errorMessage, request.getAttribute("errormessage"));
+        }
         assertEquals("/usersadminpanel", response.getForwardedUrl());
         verify(mockUserDAO);
     }
